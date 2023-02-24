99e10a90cadb05266e56a3590662b0a426d975fb
shadreza <shadreza100@gmail.com>
2023-02-25 03:08:31 +0600

    admin issue done... admin registration bug not fixed

--- src/functions/checks/security.checkAdmin.ts
+++ src/functions/checks/security.checkAdmin.ts
@@ -3,7 +3,7 @@ import { searchOneIntoFirebase } from "../auth/firebase.search"
 
 export const checkAdminState = async (passedUserMail: string, roles: string[]) => {
   const collectionName = 'users'
-  const user = await searchOneIntoFirebase(collectionName, { email: passedUserMail }, ['email', '__role__admin', '__role__master'])
+  const user = await searchOneIntoFirebase(collectionName, { email: passedUserMail }, ['email'])
   if (user) {
     if (user[0]) {
       for (let i = 0; i < roles.length; i++) { 
